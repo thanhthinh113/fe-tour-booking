cd148f54d307dd331c29388d61bceb334734d75b
ThanhDinhptkl <ntdinh25@gmail.com>
2025-04-09 14:51:03 +0700

    Create api.js file in frontend to call API

---
+++ src/api.js
@@ -0,0 +1,22 @@
+const API_BASE_URL = "http://localhost:8080/api";
+
+export const getAllTours = async () => {
+  const response = await fetch(`${API_BASE_URL}/tours`);
+  return response.json();
+};
+
+export const bookTour = async (data) => {
+  const response = await fetch(`${API_BASE_URL}/bookings`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  return response.json();
+};
+
+export const getCustomerById = async (id) => {
+  const response = await fetch(`${API_BASE_URL}/customers/${id}`);
+  return response.json();
+};
